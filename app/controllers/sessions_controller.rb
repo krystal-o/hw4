38e15f5c23b8ca761a0bb2aedcdb3b8dad0a8adc
krystal-o <krystal.onyema@kellogg.northwestern.edu>
2022-05-26 21:01:47 +0000

    added ability to sign up and log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"email"=> params["email"]})
+    if @user
+      if @user["password"] == params["password"]
+        # login the user
+        redirect_to "/places"
+      else
+        redirect_to "/sessions/new"
+      end
+    else
+        redirect_to "/sessions/new"       
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,15 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["user"]["first_name"]
-    @user["last_name"] = params["user"]["last_name"]
+    #@user["first_name"] = params["user"]["first_name"]
+    #@user["last_name"] = params["user"]["last_name"]
     @user["email"] = params["user"]["email"]
     @user["password"] = params["user"]["password"]
     @user.save
-    redirect_to "/"
+    redirect_to "/users/#{@user["id"]}"
+  end
+  
+  def show
+    @user = User.find_by({ "id" => params["id"] })
   end
 end
