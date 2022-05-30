97dde9c35839504071ea54d0463bc492ad0afb70
krystal-o <krystal.onyema@kellogg.northwestern.edu>
2022-05-30 18:50:53 +0000

    3 posts per row for large screen w/ stacking on sm

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
         session["user_id"] = @user["id"]        
         
         # login the user
-        flash["notice"] = "You've logged in."
+        flash["notice"] = "You've logged in.  |  "
         redirect_to "/places"
       else
         flash["notice"] = "Unsuccessful login."
